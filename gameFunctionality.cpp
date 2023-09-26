86db0a631a056f6cd58365893a918b0ed3a6a9ec
Andrew Prince (Student) <yxq9pb@virginia.edu>
2023-09-26 19:31:48 -0400

    test

---
+++ gameFunctionality.cpp
@@ -0,0 +1,19 @@
+#include "gameFunctionality.h"
+#include "Player.h"
+#include "Board.h"
+#include <string>
+
+
+
+gameFunctionality::gameFunctionality() {
+	this->p1 = new Player;
+	this->p2 = new Player;
+}
+
+void gameFunctionality::runGame() {
+	//test
+}
+
+string gameFunctionality::toString() {
+
+}
